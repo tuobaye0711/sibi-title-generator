c2879bf4787b95dd11108a67eb116360bcb38793
zhleven <tuobaye0711@163.com>
2018-04-29 00:03:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const sibiTitleGenerator = (title, n) => {
-    let newTitle = `《${title}》`;
+const sibiTitleGenerator = (title, n, withSymbol) => {
+    if (!withSymbol) {
+        withSymbol = true
+    }
+    let newTitle = title;
     n = Math.floor(n);
     if (n <= 0) {
-        return newTitle
+        return withSymbol ? `《${newTitle}》` : newTitle
     }
-    return sibiTitleGenerator(`驳${newTitle}`, --n)
+    return sibiTitleGenerator(`驳《${newTitle}》`, --n, withSymbol)
 }
-
 module.exports = sibiTitleGenerator;
